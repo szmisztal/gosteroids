d0d7dc2dc54091418df602f04b94f75d215bee3b
szmisztal <sz.misztal@gmail.com>
2025-07-01 12:32:07 +0200

    add rock scene

---
+++ rock/rock.gd
@@ -0,0 +1,27 @@
+extends RigidBody2D
+
+var screensize = Vector2.ZERO
+var size
+var radius
+var scale_factor = 0.2
+
+func start(_position, _velocity, _size):
+	position = _position
+	size = _size
+	mass = 1.5 * size
+	$Sprite2D.scale = Vector2.ONE * scale_factor * size
+	radius = int($Sprite2D.texture.get_size().x/2 * $Sprite2D.scale.x)
+	var shape = CircleShape2D.new()
+	shape.radius = radius
+	$CollisionShape2D.shape = shape
+	linear_velocity = _velocity
+	angular_velocity = randf_range(-PI, PI)
+	
+func _integrate_forces(physics_state):
+	var new_transform = physics_state.transform
+	new_transform.origin.x = wrapf(new_transform.origin.x, 0 - radius, screensize.x + radius)
+	new_transform.origin.y = wrapf(new_transform.origin.y, 0 - radius, screensize.y + radius)
+	physics_state.transform = new_transform
+
+func explode():
+	queue_free()
