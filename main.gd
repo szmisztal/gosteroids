de275ffd617c136a1f68e95fc17f0ee8753fd4d9
szmisztal <sz.misztal@gmail.com>
2025-07-02 12:04:31 +0200

    add explosion animation to game

--- main.gd
+++ main.gd
@@ -14,8 +14,18 @@ func spawn_rock(size, pos=null, vel=null):
 		$RockPath/RockSpawn.progress = randi()
 		pos = $RockPath/RockSpawn.position
 	if vel == null:
-		vel = Vector2.RIGHT.rotated(randf_range(0, TAU) * randf_range(50, 125))
+		vel = Vector2.RIGHT.rotated(randf_range(0, TAU)) * randf_range(50, 125)
 	var r = rock_scene.instantiate()
 	r.screensize = screensize
 	r.start(pos , vel, size)
 	call_deferred("add_child", r)
+	r.exploded.connect(self._on_rock_exploded)
+	
+func _on_rock_exploded(size, radius, pos, vel):
+	if size <= 1:
+		return
+	for offset in [-1, 1]:
+		var dir = $Player.position.direction_to(pos).orthogonal() * offset
+		var newpos = pos + dir * radius
+		var newvel = dir * vel.length() * 1.1
+		spawn_rock(size - 1, newpos, newvel)
--- rock/rock.gd
+++ rock/rock.gd
@@ -1,9 +1,10 @@
 extends RigidBody2D
 
+signal exploded
 var screensize = Vector2.ZERO
 var size
 var radius
-var scale_factor = 0.2
+var scale_factor = 0.4
 
 func start(_position, _velocity, _size):
 	position = _position
@@ -16,6 +17,7 @@ func start(_position, _velocity, _size):
 	$CollisionShape2D.shape = shape
 	linear_velocity = _velocity
 	angular_velocity = randf_range(-PI, PI)
+	$Explosion.scale = Vector2.ONE * 0.3 * size
 	
 func _integrate_forces(physics_state):
 	var new_transform = physics_state.transform
@@ -24,4 +26,12 @@ func _integrate_forces(physics_state):
 	physics_state.transform = new_transform
 
 func explode():
+	$CollisionShape2D.set_deferred("disabled", true)
+	$Sprite2D.hide()
+	$Explosion/AnimationPlayer.play("explosion")
+	$Explosion.show()
+	exploded.emit(size, radius, position, linear_velocity)
+	linear_velocity = Vector2.ZERO
+	angular_velocity = 0
+	await $Explosion/AnimationPlayer.animation_finished
 	queue_free()
